9feda009ee73ebdd2c4809e8f7cb2d4c7e58ba05
skMontajulAli <sekhmontajulali1997@gmail.com>
2023-12-24 20:46:47 +0530

    setup ddone

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import Home from './Pages/Home/Home'
+
+const App = () => {
+  return (
+    <>
+    <Home/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import { Col, Container, Row } from 'react-bootstrap'
+import './Home.scss'
+
+const Home = () => {
+  return (
+    <>
+    
+ <Container>
+    <Row>
+        <Col xxl={2}></Col>
+        <Col xxl={8}>
+
+<h1>moddle c</h1>
+
+        </Col>
+        <Col xxl={2}></Col>
+    </Row>
+ </Container>
+    </>
+  )
+}
+
+export default Home
